566e14af9e379fa480356106a76d3417633c96f7
Don Shrout <dshrout@gmail.com>
2022-02-01 06:13:59 -0600

    deleting everything bc I did the work in the starter project

--- client/src/config.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '6hr4hd137l'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
-
-export const authConfig = {
-  domain: 'dev-5muo48qz.us.auth0.com',            // Auth0 domain
-  clientId: '0NoVD4APQvdhdNw6L6jpRbdKZuaRubvl',   // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
-}
